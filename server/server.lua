4aba9c4039dc099a67be275bce240fc3ac821ea5
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2026-03-17 08:18:03 -0700

    enforce a resource name

--- server/server.lua
+++ server/server.lua
@@ -1,18 +1,30 @@
 local function loadLocale(lang)
     local content = LoadResourceFile(GetCurrentResourceName(), ('locales/%s.json'):format(lang))
     if not content then
-        print(('Locale "%s" not found, falling back to "en"'):format(lang))
+        print(('^3[jd-headbag] Locale "%s" not found, falling back to "en"^0'):format(lang))
         content = LoadResourceFile(GetCurrentResourceName(), 'locales/en.json')
     end
     local decoded = json.decode(content)
     if not decoded then
-        print('ERROR: Failed to parse locale JSON')
+        print('^1[jd-headbag] error: Failed to parse locale JSON^0')
     end
     return decoded or {}
 end
 
 local Locales = loadLocale(Config.defaultLocale)
-print(('Loaded locale "%s"'):format(Config.defaultLocale))
+print(('^2[jd-headbag] Loaded locale "%s"^0'):format(Config.defaultLocale))
+
+CreateThread(function ()
+    local name = GetCurrentResourceName()
+    if name ~= 'jd-headbag' then
+        print(('^1[jd-headbag] error: Resource must be named "jd-headbag" (currently: "%s")^0'):format(name))
+        print('^3[jd-headbag] This resource name is enforced for the following reasons:^0')
+        print('^3[jd-headbag] > respect the developer and contributors ^0')
+        print('^3[jd-headbag] > distinguish this resource from unofficial copies, forks or other resources in general ^0')
+        print('^3[jd-headbag] > ensure compatibility with exports, events, and integrations^0')
+        print('^3[jd-headbag] Please rename this resource to "jd-headbag" and restart.^0')
+    end
+end)
 
 local headbagStates = {}
 
